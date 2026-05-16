5471621c2804e66147a51e213893e88bc16b9f46
victoriamacali <72415862+victoriamacali@users.noreply.github.com>
2026-05-15 21:35:13 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import portrait from "./assets/vmac-linkedinpfp.png"
+import portrait from ".src/assets/vmac-linkedinpfp.png"
 
 export default function VictoriaMacaliWebsiteMockup() {
   const [menuOpen, setMenuOpen] = useState(false);
