6ae5abbd52a045761cdf92757f502a4eb2255197
Victoria Macali <victoriatmacali@gmail.com>
2026-05-15 22:34:48 -0700

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import portrait from ".src/assets/vmac-linkedinpfp.png"
+import portrait from "./assets/vmac-linkedinpfp.png"
 
 export default function VictoriaMacaliWebsiteMockup() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -80,12 +80,7 @@ export default function VictoriaMacaliWebsiteMockup() {
 
             <div className="w-full lg:w-[320px] bg-[#F5EFEA] text-black p-6 rounded-2xl">
               <div className="h-40 bg-gradient-to-br from-gray-200 to-gray-100 rounded-xl mb-4" />
-             <a
-  href="#"
-  className={`block text-center py-3 rounded-2xl ${pinkBtn}`}
->
-  Open PDF
-</a> 
+                  <a href="#" className={`block text-center py-3 rounded-2xl ${pinkBtn}`}>Open PDF</a>
             </div>
           </div>
         </div>
